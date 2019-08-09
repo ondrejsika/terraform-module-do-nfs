e17425c143b105144f946b5a531821a03d3ae93e
Ondrej Sika <ondrej@ondrejsika.com>
2019-08-09 09:17:02 +0200

    add working Terraform manifest

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
     2019 Ondrej Sika <ondrej@ondrejsika.com>
     https://github.com/ondrejsika/terraform-demo-nfs
+
+## Run NFS
+
+```
+terraform init
+terraform plan
+terraform apply -auto-approve
+```
+
+Server run on `nfs.sikademo.com`.
+
+
+## Stop NFS
+
+```
+terraform destroy -auto-approve
+```
---
+++ terraform.tf
@@ -0,0 +1,42 @@
+variable "do_token" {}
+variable "cloudflare_email" {}
+variable "cloudflare_token" {}
+
+provider "digitalocean" {
+  token = "${var.do_token}"
+}
+
+provider "cloudflare" {
+  email = "${var.cloudflare_email}"
+  token = "${var.cloudflare_token}"
+}
+
+data "digitalocean_droplet_snapshot" "nfs" {
+  name  = "nfs"
+  region = "fra1"
+  most_recent = true
+}
+
+
+data "digitalocean_ssh_key" "ondrejsika" {
+  name = "ondrejsika"
+}
+
+
+resource "digitalocean_droplet" "nfs" {
+  image  = "${data.digitalocean_droplet_snapshot.nfs.id}"
+  name   = "nfs"
+  region = "fra1"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.ondrejsika.id
+  ]
+}
+
+resource "cloudflare_record" "nfs" {
+  domain = "sikademo.com"
+  name   = "nfs"
+  value  = "${digitalocean_droplet.nfs.ipv4_address}"
+  type   = "A"
+  proxied = false
+}
